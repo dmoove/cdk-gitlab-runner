664e94d9b8feae88908194212d7d01e6f9aa383f
Yannick Tresch <31125132+yanu23@users.noreply.github.com>
2025-06-08 11:21:59 +0200

    feat(metric): add pending jobs scaling

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './cache';
 export * from './config-generator';
 export * from './executor';
 export * from './drain-runner';
+export * from './pending-metric';
---
+++ src/pending-metric/assets.ts
@@ -0,0 +1,3 @@
+import * as path from 'path';
+
+export const METRIC_LAMBDA_DIR = path.join(__dirname, '..', '..', 'lambda');
